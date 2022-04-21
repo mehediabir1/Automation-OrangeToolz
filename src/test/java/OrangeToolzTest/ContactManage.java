48e064607f35114415a4fbaad3cebc8e3664a11f
mehediabir1 <mehedi.abir2@gmail.com>
2022-04-21 12:38:55 +0600

    Updated 12:38

--- src/test/java/OrangeToolzTest/ContactManage.java
+++ src/test/java/OrangeToolzTest/ContactManage.java
@@ -18,9 +18,14 @@ public class ContactManage extends Base{
         GetUrl("http://159.89.38.11/login");
         Login();
         ContactManage();
+        NewContact();
+        //GroupTag_2001();
         //GroupTag_2002();
         //NumbField_2003();
-        NumbField_2004();
+        //NumbField_2004();
+        //NumbField_2005();
+        //NumbField_2006();
+        EmailField_2007();
     }
     public static void Login(){
         findByClass("form-control").sendKeys("test@orangetoolz.com");
@@ -73,7 +78,7 @@ public class ContactManage extends Base{
         }
     }
     public static void GroupTag_2002(){
-        NewContact();
+
         findByXpath("//*[@id=\"contact-form\"]/div[1]/div[1]/div/div[2]/div/a/span").click(); //Add Tag Button
         driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
         driver.findElement(By.xpath("//*[@id=\"group-add-form\"]/div[1]/div[1]/input")).sendKeys("100215"); //Tag name
@@ -99,8 +104,11 @@ public class ContactManage extends Base{
 
         }
     }
+
+    //NUMBER FIELD
+
     public static void NumbField_2003(){
-        NewContact();
+
         findByXpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/input"); //Number Input
         findByXpath("//*[@id=\"contact-form\"]/button").click();
         try{
@@ -114,15 +122,58 @@ public class ContactManage extends Base{
     }
 
     public static void NumbField_2004(){
-        NewContact();
+
+        findByXpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/input").sendKeys("012345123"); //Number Input
+        findByXpath("//*[@id=\"contact-form\"]/button").click();
+        try{
+            WebElement isDisp = driver.findElement(By.xpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/span/span"));
+            System.out.println("TC_2004 Failed");
+        }
+        catch (Exception e){
+            System.out.println("TC_2004 Passed");
+            NewContact();
+        }
+    }
+
+    public static void NumbField_2005(){
         findByXpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/input").sendKeys("abcdf"); //Number Input
         findByXpath("//*[@id=\"contact-form\"]/button").click();
         try{
             WebElement isDisp = driver.findElement(By.xpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/span/span"));
-            System.out.println("TC_2004 Passed");
+            System.out.println("TC_2005 Passed");
         }
         catch (Exception e){
-            System.out.println("TC_2004 Failed");
+            System.out.println("TC_2005 Failed");
+            NewContact();
+        }
+    }
+
+    public static void NumbField_2006(){
+        findByXpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/input").sendKeys("-1002151"); //Number Input
+        findByXpath("//*[@id=\"contact-form\"]/button").click();
+        try{
+            WebElement isDisp = driver.findElement(By.xpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/span/span"));
+            System.out.println("TC_2006 Passed");
+        }
+        catch (Exception e){
+            System.out.println("TC_2006 Failed");
+            NewContact();
+        }
+    }
+
+    //EMAIL FIELD
+
+    public static void EmailField_2007(){
+        findByXpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/input").sendKeys("1002151"); //Number Input
+        findByXpath("//*[@id=\"contact-form\"]/div[2]/div[2]/div/input"); //Email Input
+        findByXpath("//*[@id=\"contact-form\"]/button").click();
+        try{
+            WebElement isDisp = driver.findElement(By.xpath("//*[@id=\"contact-form\"]/div[2]/div[1]/div/span/span"));
+            System.out.println("TC_2006 Failed");
+        }
+        catch (Exception e){
+            System.out.println("TC_2006 Passed");
+            NewContact();
         }
     }
 }
